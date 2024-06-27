b491626bb0cc84f82a76bc8462b1ed00009ebfb0
J. Nick Koston <nick@koston.org>
2024-06-27 07:27:46 -0500

    fix: add pop functions

--- tests/test_impl.py
+++ tests/test_impl.py
@@ -1,4 +1,11 @@
-from convertertools.impl import del_dict_set, del_dict_tuple
+import pytest
+
+from convertertools.impl import (
+    del_dict_set,
+    del_dict_tuple,
+    pop_dict_set,
+    pop_dict_tuple,
+)
 
 
 def test_del_dict_tuple():
@@ -7,7 +14,44 @@ def test_del_dict_tuple():
     assert d == {"c": 3}
 
 
+def test_pop_dict_tuple():
+    d = {"a": 1, "b": 2, "c": 3}
+    pop_dict_tuple(d, ("a", "b"))
+    assert d == {"c": 3}
+
+
 def test_del_dict_set():
     d = {"a": 1, "b": 2, "c": 3}
     del_dict_set(d, {"a", "b"})
+    assert d == {"c": 3}
+
+
+def test_pop_dict_set():
+    d = {"a": 1, "b": 2, "c": 3}
+    pop_dict_set(d, {"a", "b"})
+    assert d == {"c": 3}
+
+
+def test_del_dict_tuple_missing_key():
+    d = {"a": 1, "b": 2, "c": 3}
+    with pytest.raises(KeyError):
+        del_dict_tuple(d, ("a", "b", "d"))
+    assert d == {"c": 3}
+
+
+def test_pop_dict_tuple_missing_key():
+    d = {"a": 1, "b": 2, "c": 3}
+    pop_dict_tuple(d, ("a", "b", "d"))
+    assert d == {"c": 3}
+
+
+def test_del_dict_set_missing_key():
+    d = {"a": 1, "b": 2, "c": 3}
+    with pytest.raises(KeyError):
+        del_dict_set(d, {"a", "b", "d"})
+
+
+def test_pop_dict_set_missing_key():
+    d = {"a": 1, "b": 2, "c": 3}
+    pop_dict_set(d, {"a", "b", "d"})
     assert d == {"c": 3}
