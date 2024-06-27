3f1345ba27e8fdbc48b3a74b8b32b5a46f72b73b
J. Nick Koston <nick@koston.org>
2024-06-27 07:08:08 -0500

    feat: optional delete

--- src/convertertools/impl.py
+++ src/convertertools/impl.py
@@ -8,10 +8,12 @@ from typing import Any
 def del_dict_tuple(d: dict[Any, Any], keys: tuple[Any, ...]) -> None:
     """Delete keys from a dictionary from a tuple."""
     for key in keys:
-        del d[key]
+        if key in d:
+            del d[key]
 
 
 def del_dict_set(d: dict[Any, Any], keys: set[Any]) -> None:
     """Delete keys from a dictionary from a set."""
     for key in keys:
-        del d[key]
+        if key in d:
+            del d[key]
